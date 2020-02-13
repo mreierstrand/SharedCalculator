e2288595a5b4f188d439cab20aa8cab214f2be2a
Oliver Lasse Eierstrand <oliv1115@edu.easj.dk>
2020-02-13 10:09:03 +0100

    Add project files.

---
+++ SharedCalculator/SharedCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SharedCalculator
+{
+    public class SharedCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ SharedCalculatorCalculatorTests/SharedCalculatorCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedCalculator.CalculatorTests
+{
+    [TestClass()]
+    public class SharedCalculatorCalculatorTests
+    {
+        [TestMethod()]
+        public void AddCalculatorTest()
+        {
+            SharedCalculator _calculatorTest = new SharedCalculator();
+            double result = _calculatorTest.Add(1, 2);
+            Assert.AreEqual(3,result);
+        }
+
+        [TestMethod()]
+        public void SubtractCalculatorTest()
+        {
+            SharedCalculator _calculatorTest = new SharedCalculator();
+            double result = _calculatorTest.Subtract(9, 8);
+            Assert.AreEqual(1,result);
+        }
+
+        [TestMethod()]
+        public void MultiplyCalculatorTest()
+        {
+            SharedCalculator _calculatorTest = new SharedCalculator();
+            double result = _calculatorTest.Multiply(1, 2);
+            Assert.AreEqual(2,result);
+        }
+
+        [TestMethod()]
+        public void DivideCalculatorTest()
+        {
+            SharedCalculator _calculatorTest = new SharedCalculator();
+            double result = _calculatorTest.Divide(4, 2);
+            Assert.AreEqual(2,result);
+        }
+    }
+}
